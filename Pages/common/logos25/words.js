402fa7c67ff54935490a1581f605f5a31833eddb
JithinThomasDataSemantics <thomas.jithin2@gmail.com>
2025-11-07 13:28:27 +0530

    image changes

---
+++ Pages/common/logos25/words.js
@@ -0,0 +1,12 @@
+wordsData={
+  "ec": {"display":"സഭാപ്രസംഗകൻ",
+    "1": {
+      "ec1.1": "The words of the Teacher, son of David, king in Jerusalem:",
+      "ec1.2": "Vanity of vanities, says the Teacher, vanity of vanities! All is vanity."
+    },
+    "2": {
+      "ec2.1": "I said to myself, 'Come now, I will test you with pleasure...'",
+      "ec2.2": "Laughter, I said, is madness. And what does pleasure accomplish?"
+    }
+  }
+}
